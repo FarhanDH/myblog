f5012fa7e8f487e2d1fd9eedbe2da5f1d8590f34
Farhan Dwi Hartanto <farhandwihartanto@gmail.com>
2024-03-31 14:15:55 +0800

    feat(client/homePage): create homepage

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Header } from './Header';
+import { Post } from './Post';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <Header />
+        <Post />
+        <Post />
+        <Post />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Header.jsx
@@ -0,0 +1,13 @@
+export function Header(params) {
+  return (
+    <header>
+      <a href="" className="logo">
+        MyBlogs
+      </a>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+  );
+}
